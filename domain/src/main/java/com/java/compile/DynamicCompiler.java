849758b80f01caaba8c5f139c521ee606a87c6f9
leiline <lilin@lilindeMacBook-Pro.local>
2019-08-25 18:03:06 +0800

    判断当前系统

---
+++ domain/src/main/java/com/java/compile/DynamicCompiler.java
@@ -0,0 +1,21 @@
+package com.java.compile;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+/***
+ * 代码动态变异
+ */
+public class DynamicCompiler {
+
+    public static boolean compile(String code) {
+
+        // ToolProvider 类似于一个工具箱
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
