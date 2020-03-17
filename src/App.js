41ed41d9cfdf9885e7c94bf581c1b4ed5a65d18a
leiline <lamlee1993@outlook.com>
2020-03-17 22:16:21 +0800

    Add: add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 
-import {
-    Button
-} from 'antd'
+import { adminRouter } from './routes'
+
+import {Route, Switch} from 'react-router-dom'
 
 
 export default class App extends Component {
     render() {
         return(
             <div>
-                app <Button type="primary">ANTD</Button>
+                <div>这里是公共的部分</div>
+                <Switch>
+                    {
+                        adminRouter.map(
+                            route => {
+                                return <Route key={route.pathname} path={route.pathname} render={(routerProps) => {
+                                    return <route.component {...routerProps} />
+                                }}/>
+                            }
+                        )
+                    }
+                </Switch>
             </div>
         )
     }
--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,43 @@
-import Dashboard from './Dashboard'
-import Login from './Login'
-import NotFound from './NotFound'
-import Settings from './Settings'
-import ArticleList from './Article'
-import ArticleEdit from './Article/Edit'
+import loadable from 'react-loadable'
+import { Loading } from '../components/Loading'
+// import Dashboard from './Dashboard'
+// import Login from './Login'
+// import NotFound from './NotFound'
+// import Settings from './Settings'
+// import ArticleList from './Article'
+// import ArticleEdit from './Article/Edit'
+
+
+// this is lazy loading 
+const Dashboard = Loadable({
+    loader: () => import('./Dashboard'),
+    loading: Loading
+})
+
+const NotFound = Loadable({
+    loader: () => import('./NotFound'),
+    loading: Loading
+})
+
+const Settings = Loadable({
+    loader: () => import('./Settingsd'),
+    loading: Loading
+})
+
+const Login = Loadable({
+    loader: () => import('./Login'),
+    loading: Loading
+})
+
+const ArticleList = Loadable({
+    loader: () => import('./Article'),
+    loading: Loading
+})
+
+const ArticleEdit = Loadable({
+    loader: () => import('./Article/Edit'),
+    loading: Loading
+})
 
 export {
     Dashboard,
